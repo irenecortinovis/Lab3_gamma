9530f332c7333857735b2ebe45d8be3becb4a093
mmarchegiani <matmarcheg@gmail.com>
2018-05-17 23:54:25 +0200

    Angular correlation plot

---
+++ Coincidence/Na22/angles.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#Script per generare il TGraph counts_vs_theta
+#Viene creato il file Na22_correlation.txt
+touch Na22_correlation.txt
+echo "Theta	N_counts errTheta errN_counts" > Na22_correlation.txt
+echo -n "0" >> Na22_correlation.txt
+cd deg00
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "5" >> Na22_correlation.txt
+cd deg05
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "10" >> Na22_correlation.txt
+cd deg10
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "15" >> Na22_correlation.txt
+cd deg15
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "20" >> Na22_correlation.txt
+cd deg20
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "25" >> Na22_correlation.txt
+cd deg25
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "30" >> Na22_correlation.txt
+cd deg30
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "45" >> Na22_correlation.txt
+cd deg45
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "60" >> Na22_correlation.txt
+cd deg60
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+echo -n "75" >> Na22_correlation.txt
+cd deg75
+../../../Code/easyfit.o histo.dat r pet
+cd ..
+#echo "3" >> Na22_correlation.txt
+
+cat Na22_correlation.txt
---
+++ Coincidence/Na22/correlation.cpp
@@ -0,0 +1,61 @@
+/*
+compile with:
+g++ correlation.cpp -o correlation.o `root-config --cflags --glibs`
+*/
+
+//This program fits the angular correlation graph of 511 keV back to back emission with the Fermi-Dirac function
+
+#include <fstream>
+#include <TMultiGraph.h>
+#include <TGraph.h>
+#include <TGraphErrors.h>
+#include <TCanvas.h>
+#include <TApplication.h>
+#include <TStyle.h>
+#include <TROOT.h>
+#include <string>
+#include <TLegend.h>
+
+
+int main(int argc, char **argv) {
+	//double alpha = argv[1];
+	TApplication* Grafica = new TApplication("Grafica", 0, NULL);
+	TGraphErrors *angles = new TGraphErrors("Na22_correlation.txt", "%lg %lg %lg");
+	double *x = angles->GetX();
+	for(int i = 0; i < angles->GetN(); i++) {
+		angles->SetPointError(i, 3, sqrt(x[i]));		//(i, ex, ey)
+	}
+
+	TF1 *fermidirac = new TF1("fd", "[0]/(exp([1]*(x-[2])) + 1)", 0., 30.);
+	TF1 *cos2 = new TF1("cos2", "[0]*cos([1]*x)**2 + [2]", 0., 30.);
+	fermidirac->SetParName(0, "N_{0}");
+	fermidirac->SetParName(1, "#alpha");
+	fermidirac->SetParName(2, "#theta_{0}");
+	fermidirac->SetParameters(408710., 0.446, 14.);
+	//fermidirac->FixParameter(2, 14.);
+	cos2->SetParName(0, "N_{0}");
+	cos2->SetParName(1, "#omega");
+	cos2->SetParName(2, "N_{offset}");
+	cos2->SetParameters(410000., 0.1, 1800.);
+	//TLegend *legend = new TLegend(0.65, 0.65, 0.85, 0.85);
+	angles->SetTitle("Angular correlation 511 keV; #theta (#circ); N_{counts}");
+	angles->SetMarkerColor(kBlue);
+	angles->SetLineColor(kBlue);
+	angles->SetMarkerStyle(7);
+	angles->SetMarkerSize(5);
+
+
+	//legend->AddEntry(angles, "511keV", "l");
+	gStyle->SetOptFit(1111);
+
+	TCanvas *c1 = new TCanvas("counts_vs_theta","counts_vs_theta",800,600);
+	angles->Draw("ape");
+	//fermidirac->Draw("same");
+	angles->Fit("fd", "R");
+	//angles->Fit("cos2", "R");
+	//legend->Draw();
+	c1->Print("counts_vs_theta.png");
+
+	Grafica->Run();
+	return 0;
+}
