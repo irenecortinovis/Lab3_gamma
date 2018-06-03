f89b34fa762da907336cf480062394c2c9ac7380
mmarchegiani <matmarcheg@gmail.com>
2018-06-03 15:35:33 +0200

    Definitive config.txt files

--- Coincidence/Co60/angles.sh
+++ Coincidence/Co60/angles.sh
@@ -2,9 +2,9 @@
 #Script per generare il TGraph counts_vs_theta
 #Viene creato il file Co60_correlation.txt
 touch Co60_correlation.txt
-echo "Theta	peak1	peak2	sb1		sb2" > Co60_correlation.txt
+echo "Theta	peak1	peak2" > Co60_correlation.txt
 echo -n "180" >> Co60_correlation.txt
-cd deg00
+cd deg0_retake
 ../../../Code/easyfit.o histo.dat r Co60
 cd ..
 
@@ -13,10 +13,10 @@ cd deg10
 ../../../Code/easyfit.o histo.dat r Co60
 cd ..
 
-#echo -n "200" >> Co60_correlation.txt
-#cd deg20_retake2
-#../../../Code/easyfit.o histo.dat r Co60
-#cd ..
+echo -n "200" >> Co60_correlation.txt
+cd deg20_retake2
+../../../Code/easyfit.o histo.dat r Co60
+cd ..
 
 echo -n "210" >> Co60_correlation.txt
 cd deg30
@@ -58,5 +58,5 @@ cd HPGe_alone
 cd ..
 
 cat Co60_correlation.txt
-echo "ratio = $(cat ratio.txt)"
-./correlation.o
+#echo "ratio = $(cat ratio.txt)"
+#./correlation.o
--- Coincidence/Co60/correlation.cpp
+++ Coincidence/Co60/correlation.cpp
@@ -18,85 +18,52 @@ g++ correlation.cpp -o correlation.o `root-config --cflags --glibs`
 #include <cmath>
 
 int main(int argc, char **argv) {
+	if(argc < 2) {
+		std::cout << "Errore: inserire file sorgente come argomento." << std::endl;
+		return 1;
+	}
+
 	const int n = 20;
-	double theta[n], count1[n], count2[n], count2_corr[n], err_corr[n], count2_acc[n], err_acc[n],
-	count1_sb[n], count2_sb[n], count2_corr_sb[n], err_corr_sb[n], count2_acc_sb[n], err_acc_sb[n],
-	count2_norm[n], err_norm[n];
+	double theta[n], count1[n], count2[n], count2_corr[n], err_corr[n], count2_acc[n], err_acc[n];
 	double ratio, err_ratio;
 	std::ifstream ratiofile("ratio.txt");
 	ratiofile >> ratio >> err_ratio;
 	ratiofile.close();
-	std::ifstream infile("Co60_correlation.txt");
+
+	std::ifstream infile(argv[1]);
 	int j = 0;
 	char buffer[20];
-	infile >> buffer >> buffer >> buffer >> buffer >> buffer;
-	//std::cout << "Calcolo dell'errore con 2 metodi: statistica di Poisson e propagazione degli errori (errori su N1, N2, ratio)" << std::endl;
-	//std::cout << "theta\tpoisson\tpropagation\tvariation" << std::endl;
-	double mean_peak1 = 0;
+	infile >> buffer >> buffer >> buffer;
 	while(infile.good()) {
-		infile >> theta[j] >> count1[j] >> count2[j] >> count1_sb[j] >> count2_sb[j];
+		infile >> theta[j] >> count1[j] >> count2[j];
 		count2_acc[j] = ratio*count1[j];
 		count2_corr[j] = count2[j] - count2_acc[j];		//Sottraggo dai conteggi del picco 2 le false coincidenze
 		err_acc[j] = count2_acc[j]*sqrt(pow(err_ratio/ratio, 2) + 1./count1[j]);
 		err_corr[j] = sqrt(count2[j] + pow(err_acc[j], 2));
-
-		count2_acc_sb[j] = ratio*count1_sb[j];
-		count2_corr_sb[j] = count2_sb[j] - count2_acc_sb[j];		//Sottraggo dai conteggi del picco 2 le false coincidenze
-		err_acc_sb[j] = count2_acc_sb[j]*sqrt(pow(err_ratio/ratio, 2) + 1./count1_sb[j]);
-		err_corr_sb[j] = sqrt(count2_sb[j] + pow(err_acc_sb[j], 2));
-		
-		//count2_norm[j] = count2_corr_sb[j]/count1_sb[j];
-		//err_norm[j] = count2_norm[j]*sqrt(pow(err_corr_sb[j]/count2_corr_sb[j], 2) + 1./count1_sb[j]);
-
-		//std::cout << theta[j] << "\t" << sqrt(count2_corr[j]) << "\t" << err_corr[j];
-		//std::cout << "\t\t+" << 100*(err_corr[j] - sqrt(count2_corr[j]))/sqrt(count2_corr[j]) << "%"<< std::endl;
-		mean_peak1 += count1[j];
 		j++;
 	}
 	infile.close();
 	int npoints = j;
-	mean_peak1 /= (npoints - 1);
-
-	for(j = 0; j < npoints-1; j++) {
-		count2_norm[j] = count2_corr_sb[j]/mean_peak1;
-		err_norm[j] = count2_norm[j]*sqrt(pow(err_corr_sb[j]/count2_corr_sb[j], 2) + 1./mean_peak1);
-	}
 
 	TApplication* Grafica = new TApplication("Grafica", 0, NULL);
 	TGraphErrors *angles = new TGraphErrors(npoints-1, theta, count2_corr);
-	TGraphErrors *angles_sb = new TGraphErrors(npoints-1, theta, count2_corr_sb);
-	TGraphErrors *angles_norm = new TGraphErrors(npoints-1, theta, count2_norm);
 	double *y = angles->GetY();
 	for(int i = 0; i < angles->GetN(); i++) {
 		angles->SetPointError(i, 1, err_corr[i]);			//(i, ex, ey)
-		angles_sb->SetPointError(i, 1, err_corr_sb[i]);		//(i, ex, ey)
-		angles_sb->SetPointError(i, 1, err_norm[i]);		//(i, ex, ey)
 	}
 
 	//TF1 *cos2 = new TF1("cos2", "[0]*([1]*(cos(x/180*pi))**2 + [2]*(cos(x/180*pi))**4) + [3]", 180., 270.);
 	TF1 *cos2 = new TF1("cos2", "[0]*([1]*(cos(pi*x/180))**2 + [2]*(cos(pi*x/180))**4 + 1)", 180., 270.);
-	TF1 *cos2_sb = new TF1("cos2_sb", "[0]*([1]*(cos(pi*x/180))**2 + [2]*(cos(pi*x/180))**4 + 1)", 180., 270.);
-	TF1 *cos2_norm = new TF1("cos2_norm", "[0]*([1]*(cos(pi*x/180))**2 + [2]*(cos(pi*x/180))**4 + 1)", 180., 270.);
 	cos2->SetParName(0, "N_{0}");
-	cos2->SetParName(1, "a1");
-	cos2->SetParName(2, "a2");
+	cos2->SetParName(1, "a_{1}");
+	cos2->SetParName(2, "a_{2}");
 
-	cos2_sb->SetParName(0, "N_{0}");
-	cos2_sb->SetParName(1, "a1");
-	cos2_sb->SetParName(2, "a2");
-	
-	cos2_norm->SetParName(0, "N_{0}");
-	cos2_norm->SetParName(1, "a1");
-	cos2_norm->SetParName(2, "a2");
-	
 	//cos2->SetParName(3, "offset");
-	cos2->SetParameters(6400, 0.125, 0.04);
-	cos2_sb->SetParameters(6400, 0.125, 0.04);
-	cos2_norm->SetParameters(6400, 0.125, 0.04);
+	cos2->SetParameters(20, 0.125, 0.04);
 	//cos2->FixParameter(0, 1794);
 	//cos2->FixParameter(1, 0.125);
 	//cos2->FixParameter(2, 0.042);
-	//cos2->SetParLimits(1, 0.118, 0.138);
+	//cos2->SetParLimits(1, 0.118, 0.132);
 	//cos2->SetParLimits(2, 0.038, 0.044);
 	angles->SetTitle("Angular correlation #gamma-#gamma ^{60}Co; #theta (#circ); N_{counts}");
 	angles->SetMarkerColor(kBlue);
@@ -104,31 +71,22 @@ int main(int argc, char **argv) {
 	angles->SetMarkerStyle(7);
 	angles->SetMarkerSize(5);
 
-	angles_sb->SetTitle("Angular correlation signal + background; #theta (#circ); N_{counts}");
-	angles_sb->SetMarkerColor(kBlue);
-	angles_sb->SetLineColor(kBlue);
-	angles_sb->SetMarkerStyle(7);
-	angles_sb->SetMarkerSize(5);
-
-	angles_norm->SetTitle("Angular correlation normalized; #theta (#circ); N_{corr} / N_{1}");
-	angles_norm->SetMarkerColor(kBlue);
-	angles_norm->SetLineColor(kBlue);
-	angles_norm->SetMarkerStyle(7);
-	angles_norm->SetMarkerSize(5);
-
 	gStyle->SetOptFit(1111);
 
 	TCanvas *c1 = new TCanvas("counts_vs_theta","counts_vs_theta",800,600);
-	c1->Divide(2, 2);
-	c1->cd(1);
 	angles->Draw("ape");
 	angles->Fit("cos2", "R");
-	c1->cd(2);
-	angles_sb->Draw("ape");
-	angles_sb->Fit("cos2_sb", "R");
-	c1->cd(3);
-	angles_norm->Draw("ape");
-	angles_norm->Fit("cos2_norm", "R");
+	TF1 *cos2_exp = new TF1("cos2", "[0]*([1]*(cos(pi*x/180))**2 + [2]*(cos(pi*x/180))**4 + 1)", 180., 270.);
+	cos2_exp->SetParameter(0, (angles->GetFunction("cos2"))->GetParameter(0));
+	cos2_exp->SetParameter(1, 0.125);
+	cos2_exp->SetParameter(2, 0.042);
+	cos2_exp->SetLineStyle(7);
+	cos2_exp->SetLineColor(kGray);
+	cos2_exp->Draw("same");
+	TLegend *legend = new TLegend(0.15, 0.15, 0.3, 0.3);
+	legend->AddEntry(cos2, "Fit", "l");
+	legend->AddEntry(cos2_exp, "Theoric", "l");
+	legend->Draw();
 	c1->Print("counts_vs_theta.png");
 
 	Grafica->Run();
