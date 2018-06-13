140bb24cf142e450cedd5d6a7c74f865e88101be
Irene Cortinovis <irenecortinovis1@gmail.com>
2018-06-13 23:14:36 +0200

    Add labels to Fano

--- HPGe/Fano/FWHM.cpp
+++ HPGe/Fano/FWHM.cpp
@@ -12,6 +12,7 @@ g++ FWHM.cpp -o FWHM.o `root-config --cflags --glibs`
 #include <TROOT.h>
 #include <string>
 #include <TLegend.h>
+#include <TLatex.h>
 
 
 int main() {
@@ -25,13 +26,30 @@ int main() {
 	fwhm->SetMarkerSize(5);
 
 
-  TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x) + [2]*(x**2)) ", 0, 1600.);
+  //TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x) + [2]*(x**2)) ", 0, 1600.);
+  TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x)) ", 0, 1600.);
 	fitfano->SetParName(0, "b^{2}");
   fitfano->SetParName(1, "a^{2}");
-  fitfano->SetParName(2, "c^{2}");
+  //fitfano->SetParName(2, "c^{2}");
   fitfano->SetParameter(0,1);
-  //fitfano->SetParameter(1,1);
-	//fitfano->SetParameter(2,0);
+
+
+  TLatex *latex0 = new TLatex(fwhm->GetX()[0], fwhm->GetY()[0],"Na22");
+  TLatex *latex1 = new TLatex(fwhm->GetX()[1], fwhm->GetY()[1],"Na22");
+  TLatex *latex2 = new TLatex(fwhm->GetX()[2], fwhm->GetY()[2],"K40");
+  TLatex *latex3 = new TLatex(fwhm->GetX()[3], fwhm->GetY()[3],"Co60");
+  TLatex *latex4 = new TLatex(fwhm->GetX()[4], fwhm->GetY()[4],"Co60");
+  TLatex *latex5 = new TLatex(fwhm->GetX()[5], fwhm->GetY()[5],"Th228");
+  TLatex *latex6 = new TLatex(fwhm->GetX()[6], fwhm->GetY()[6],"Th228");
+  TLatex *latex7 = new TLatex(fwhm->GetX()[7], fwhm->GetY()[7],"Th228");
+  fwhm->GetListOfFunctions()->Add(latex0);
+  fwhm->GetListOfFunctions()->Add(latex1);
+  fwhm->GetListOfFunctions()->Add(latex2);
+  fwhm->GetListOfFunctions()->Add(latex3);
+  fwhm->GetListOfFunctions()->Add(latex4);
+  fwhm->GetListOfFunctions()->Add(latex5);
+  fwhm->GetListOfFunctions()->Add(latex6);
+  fwhm->GetListOfFunctions()->Add(latex7);
 
   /*TF1 *fitfano = new TF1("fitfano", "2.35*sqrt([0]*2.96*x) ", 0, 1600.);
 	fitfano->SetParName(0, "F");
@@ -44,6 +62,14 @@ int main() {
 	fwhm->Draw("ape");
 	//legend->Draw();
   fwhm->Fit("fitfano", "R");
+
+  double asquare = fitfano->GetParameter(1);
+  double asquare_err = fitfano->GetParError(1);
+
+  double fanofactor = asquare / (2.35*2.35*2.96);
+  double fanofactor_err = asquare_err / (2.35*2.35*2.96);
+
+  std::cout << "Fano factor = \t" << fanofactor << "\t +- \t" << fanofactor_err << std::endl;
 
 	c1->Print("FWHM_vs_Energy.png");
 
--- NaI/Fano/FWHM.cpp
+++ NaI/Fano/FWHM.cpp
@@ -12,6 +12,7 @@ g++ FWHM.cpp -o FWHM.o `root-config --cflags --glibs`
 #include <TROOT.h>
 #include <string>
 #include <TLegend.h>
+#include <TLatex.h>
 
 
 int main() {
@@ -25,18 +26,38 @@ int main() {
 	fwhm->SetMarkerSize(5);
 
 
-  /*TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x) + [2]*(x**2)) ", 0, 1600.);
+  TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x) + [2]*(x**2)) ", 0, 1600.);
 	fitfano->SetParName(0, "b^{2}");
   fitfano->SetParName(1, "a^{2}");
   fitfano->SetParName(2, "c^{2}");
   fitfano->SetParameter(0,0);
   fitfano->SetParameter(1,0);
-	fitfano->SetParameter(2,0);*/
+	fitfano->SetParameter(2,0);
 
 
-  TF1 *fitfano = new TF1("fitfano", "sqrt([0]*2.96*x) ", 0, 1600.);
+  TLatex *latex0 = new TLatex(fwhm->GetX()[0], fwhm->GetY()[0],"Na22");
+  TLatex *latex1 = new TLatex(fwhm->GetX()[1], fwhm->GetY()[1],"Na22");
+  TLatex *latex2 = new TLatex(fwhm->GetX()[2], fwhm->GetY()[2],"Co60");
+  TLatex *latex3 = new TLatex(fwhm->GetX()[3], fwhm->GetY()[3],"Co60");
+  TLatex *latex4 = new TLatex(fwhm->GetX()[4], fwhm->GetY()[4],"Th228");
+  TLatex *latex5 = new TLatex(fwhm->GetX()[5], fwhm->GetY()[5],"Th228");
+  fwhm->GetListOfFunctions()->Add(latex0);
+  fwhm->GetListOfFunctions()->Add(latex1);
+  fwhm->GetListOfFunctions()->Add(latex2);
+  fwhm->GetListOfFunctions()->Add(latex3);
+  fwhm->GetListOfFunctions()->Add(latex4);
+  fwhm->GetListOfFunctions()->Add(latex5);
+
+  /*TF1 *fitfano = new TF1("fitfano", "sqrt([0] + [1]*(x)) ", 0, 1600.);
 	fitfano->SetParName(0, "b^{2}");
-	fitfano->SetParameter(0,0.1);
+  fitfano->SetParName(1, "a^{2}");
+  fitfano->SetParameter(0,0);
+  fitfano->SetParameter(1,0);*/
+
+
+  /*TF1 *fitfano = new TF1("fitfano", "sqrt([0]*2.96*x) ", 0, 1600.);
+	fitfano->SetParName(0, "b^{2}");
+	fitfano->SetParameter(0,0.1);*/
 
 
 	gStyle->SetOptFit(1111);
@@ -45,6 +66,16 @@ int main() {
 	fwhm->Draw("ape");
 	//legend->Draw();
   fwhm->Fit("fitfano", "R");
+
+
+      double asquare = fitfano->GetParameter(1);
+      double asquare_err = fitfano->GetParError(1);
+
+      double fanofactor = asquare / (2.35*2.35*2.96);
+      double fanofactor_err = asquare_err / (2.35*2.35*2.96);
+
+      std::cout << "Fano factor = \t" << fanofactor << "\t +- \t" << fanofactor_err << std::endl;
+
 
 	c1->Print("FWHM_vs_Energy.png");
 
